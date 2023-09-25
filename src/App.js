e805e9cfd6b286c30c0f7d4acaad3ba8d71dc50f
Gangaithiri241991 <ajimansamy29@gmail.com>
2023-09-25 11:47:47 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import './Assets/css/sb-admin-2.min.css';
+import Page from "./page";
+function App() {
+  return (
+    <div className="App">
+<Page />
+    </div>
+  )
+    
+       
+  
+}
+
+export default App;
